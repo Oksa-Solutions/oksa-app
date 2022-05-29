1c682578dc17cab4dc10b83bff033edef7d1d705
Jore Rautava <jorerautava@protonmail.com>
2022-05-29 19:34:39 +0300

    Add dropdown listing of available languages

--- Frontend/components/common/languageSelection.vue
+++ Frontend/components/common/languageSelection.vue
@@ -1,10 +1,9 @@
 <template>
-  <v-menu
-      offset-y
-      transition="scale-transition"
-  >
+  <v-menu offset-y transition="scale-transition">
     <template v-slot:activator="{on, attrs}">
-      <Flag v-bind="attrs" v-on="on" :code="lang" />
+      <div v-bind="attrs" v-on="on" class="elevation-0 px-2 rounded-lg">
+        <Flag :code="lang" hasDropShadow />
+      </div>
     </template>
     <v-list>
       <v-list-item
@@ -12,27 +11,28 @@
         :key="i"
         @click="setLanguage(l)"
       >
-        <v-icon>mdi-cog</v-icon>
-        <Flag :code="l" :hasDropShadow="lang===l"/>
+        <Flag :code="l" :hasDropShadow="lang === l" />
       </v-list-item>
     </v-list>
   </v-menu>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import Component from 'vue-class-component';
+//import Vue from 'vue';
+//import Component from 'vue-class-component';
+import {Vue, Component} from 'vue-property-decorator';
+// import {Vue, Component} from 'vue-property-decorator';
 import {mapState} from 'vuex';
-import { LANGUAGES } from '~/assets/languages';
-import { SET_LANG } from '~/store/mutationTypes';
+import {LANGUAGES} from '~/assets/languages';
+import {SET_LANG} from '~/store/mutationTypes';
 
 @Component({
   computed: mapState({
     lang: (state: any) => state.modules.base.language,
-  })
+  }),
 })
 export default class LanguageSelection extends Vue {
-  availableLanguages = Object.keys(LANGUAGES);
+  availableLanguages = Object.keys(LANGUAGES) || [];
 
   setLanguage(newLang: string) {
     this.$store.commit(`modules/base/${SET_LANG}`, newLang);
