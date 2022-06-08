2eeb3e13e2b2892679057e0fe381284ccf1a4d66
Jore Rautava <jorerautava@protonmail.com>
2022-06-08 20:21:16 +0300

    Add function type to Vue instance

--- Frontend/plugins/getLanguageVersions.ts
+++ Frontend/plugins/getLanguageVersions.ts
@@ -2,6 +2,12 @@ import {Context} from '@nuxt/types';
 import {Inject} from '@nuxt/types/app';
 import {LANGUAGES} from '~/assets/languages';
 
+declare module 'vue/types/vue' {
+  interface Vue {
+    $setContent(content: string): string
+  }
+}
+
 export default (context: Context, inject: Inject) => {
   inject('setContent', (content: string) => {
     return LANGUAGES[context.store.state.modules.base.language][content];
